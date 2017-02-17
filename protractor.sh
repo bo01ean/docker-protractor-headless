b26477f21821ff2469d563b5cae3266183748c50
Nathan Trujillo <NathanTrujillo@shop.local>
2017-02-16 16:45:48 -0800

    Fix so Docker doesn't pollute your working directory.

--- protractor.sh
+++ protractor.sh
@@ -2,9 +2,8 @@
 npm install
 chmod 777 /protractor
 su testerguy -c "
-
+  TMPDIR=/tmp
   dbus-launch --exit-with-session
   echo \"$@:\"
-  xvfb-run -f /tmp/ -a --server-args=\"-screen 0 ${SCREEN_RES}\" protractor $@
-
+  xvfb-run -a --server-args=\"-screen 0 ${SCREEN_RES}\" protractor $@
 "
