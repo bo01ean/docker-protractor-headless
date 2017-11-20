352b88a17e4d1a0484de54ee53ecf702a0d1e947
Nathan Trujillo <ntrujillo@illumina.com>
2017-11-20 12:01:47 -0800

    Allow command to be passed in from docker runner.

--- runtests.sh
+++ runtests.sh
@@ -4,7 +4,6 @@ set -e
 chmod 777 /protractor
 su testerguy -c "
   TMPDIR=/tmp
-  # whoami
-  # sudo whoami
-  DBUS_SESSION_BUS_ADDRESS=/dev/null xvfb-run -a --server-args=\"-screen 0 ${SCREEN_RES}\" gulp test
+  echo \"   ---- Running: XVFB with: '$@' ----\"
+  DBUS_SESSION_BUS_ADDRESS=/dev/null xvfb-run -a --server-args=\"-screen 0 ${SCREEN_RES}\" $@
 "
